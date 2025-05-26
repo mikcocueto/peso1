9c4673675e05b7493be0a59c7f4999b676b1275d
mikcocueto <148319845+ShanShenShan@users.noreply.github.com>
2025-05-26 16:12:01 +0800

    Catcher in the UI by Ui Shigure

--- index.php
+++ index.php
@@ -444,12 +444,10 @@ $conn->close();
           <div class="col-6 col-md-3 mb-4 mb-md-0">
             <h3>Search Trending</h3>
             <ul class="list-unstyled">
-              <li><a href="#">Web Design</a></li>
-              <li><a href="#">Graphic Design</a></li>
-              <li><a href="#">Web Developers</a></li>
-              <li><a href="#">Python</a></li>
-              <li><a href="#">HTML5</a></li>
-              <li><a href="#">CSS3</a></li>
+              <li><a href="#">Accounting</a></li>
+              <li><a href="#">Health Care</a></li>
+              <li><a href="#">Information Technology</a></li>
+              <li><a href="#">Engineering</a></li>
             </ul>
           </div>
           <div class="col-6 col-md-3 mb-4 mb-md-0">
